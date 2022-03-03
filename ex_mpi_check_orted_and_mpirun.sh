e0a33a5cf3f822cf77288ad6e554b50f6134bdb5
Min-Su Shin <msshin@kasi.re.kr>
2022-03-03 10:57:16 +0900

    New script to check availability of minion nodes.

---
+++ ex_mpi_check_orted_and_mpirun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CLUSTERNAME="mycluster"
+MINIONLASTIND="14"
+
+for ind in $(seq 0 ${MINIONLASTIND})
+do
+  res=$(ssh ${CLUSTERNAME}-minion-${ind} "which orted mpirun" | wc -l)
+  if [ ${res} -ne "2" ]
+  then
+    echo "[WARNING] ${CLUSTERNAME}-minion-${ind} is not ready yet."
+  fi
+done
