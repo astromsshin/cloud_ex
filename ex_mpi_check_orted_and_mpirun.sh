75e9aff7ca7d7510e14334f9194cee442e1bcfd6
Min-Su Shin <msshin@kasi.re.kr>
2022-03-03 10:58:44 +0900

    Echoing... in the script.

--- ex_mpi_check_orted_and_mpirun.sh
+++ ex_mpi_check_orted_and_mpirun.sh
@@ -5,6 +5,7 @@ MINIONLASTIND="14"
 
 for ind in $(seq 0 ${MINIONLASTIND})
 do
+  echo "... checking ${CLUSTERNAME}-minion-${ind}"
   res=$(ssh ${CLUSTERNAME}-minion-${ind} "which orted mpirun" | wc -l)
   if [ ${res} -ne "2" ]
   then
