c003a5a943d9566fb73da4a8d89a6b7f6fb8d6e9
Min-Su Shin <msshin@kasi.re.kr>
2022-03-03 10:51:18 +0900

    Updating the MPI files.

--- ex_mpi_montecarlo_pi.cpp
+++ ex_mpi_montecarlo_pi.cpp
@@ -8,8 +8,8 @@
 int main(int argc, char** argv) {
 
   long int i, niter = 50000000;
-  int count = 0;
-  int myrank, num_procs;
+  long int count = 0;
+  int proc_ind, myrank, num_procs;
   double x,y, z, pi;
                                
   MPI_Init(&argc, &argv);
@@ -34,19 +34,19 @@ int main(int argc, char** argv) {
 
   /* Collect the results on the MPI process 0 */
   if(myrank != 0) {
-    MPI_Send(&count, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    MPI_Send(&count, 1, MPI_LONG_INT, 0, 1, MPI_COMM_WORLD);
   } else {
     for(i = 1; i < num_procs; ++i) {
-      MPI_Recv(&recieved_result[i], num_procs, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&recieved_result[i], num_procs, MPI_LONG_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
   }
                                                                
   if (myrank == 0) {
 
-    int final_count = 0;
+    long int final_count = 0;
 
-    for(i = 1; i < num_procs; ++i) {
-      final_count += recieved_result[i];
+    for(proc_ind = 1; proc_ind < num_procs; ++proc_ind) {
+      final_count += recieved_result[proc_ind];
     }
     final_count += count;
 
