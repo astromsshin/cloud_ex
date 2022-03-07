c7cbdf5b361649c94eecfaead6b61ee589d48479
Min-Su Shin <msshin@kasi.re.kr>
2022-03-07 12:36:12 +0900

    Chaning the check shell script for slurm.

--- ex_mpi_check_munged_and_mpirun.sh
+++ ex_mpi_check_munged_and_mpirun.sh
@@ -6,7 +6,7 @@ MINIONLASTIND="14"
 for ind in $(seq 0 ${MINIONLASTIND})
 do
   echo "... checking ${CLUSTERNAME}-minion-${ind}"
-  res=$(ssh ${CLUSTERNAME}-minion-${ind} "which orted mpirun" | wc -l)
+  res=$(ssh ${CLUSTERNAME}-minion-${ind} "which munged mpirun" | wc -l)
   if [ ${res} -ne "2" ]
   then
     echo "[WARNING] ${CLUSTERNAME}-minion-${ind} is not ready yet."
