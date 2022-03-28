a8ce9cbf0a78dcfb8ee5b54f6b941cd8cba3cce8
Min-Su Shin <msshin@kasi.re.kr>
2022-03-28 20:08:16 +0900

    Some useful scripts.

---
+++ tools_chage_password_all_nodes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLUSTERNAME="mycluster"
+MINIONLASTIND="14"
+PWUSER="root"
+NEWPASSWORD="xxxxxxxxxx"
+
+echo "... changing ${CLUSTERNAME}-master : ${PWUSER}"
+echo -e "${NEWPASSWORD}\n${NEWPASSWORD}" | passwd ${PWUSER}
+
+for ind in $(seq 0 ${MINIONLASTIND})
+do
+  echo "... changing ${CLUSTERNAME}-minion-${ind} : ${PWUSER}"
+  ssh ${CLUSTERNAME}-minion-${ind} echo -e "${NEWPASSWORD}\n${NEWPASSWORD}" | passwd ${PWUSER}
+done
