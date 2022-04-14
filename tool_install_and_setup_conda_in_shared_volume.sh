23fd5b97ad8fab055f89b656e6a4898df6cc88d4
Min-Su Shin <msshin@kasi.re.kr>
2022-04-14 12:58:19 +0900

    New script to setup Conda environment.

---
+++ tool_install_and_setup_conda_in_shared_volume.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CLUSTERNAME="mycluster"
+MINIONLASTIND="14"
+NFSDIR="/mnt/mpi"
+
+CONDAENV="xclass"
+CONDAURL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+
+# installation of miniconda
+cd ${NFSDIR}
+wget "${CONDAURL}" -O ./miniconda.sh
+bash ./miniconda.sh -b -p ${NFSDIR}/miniconda
+
+eval "$(${NFSDIR}/miniconda/bin/conda shell.bash hook)"
+conda init
+conda update -y -n base -c defaults conda
+
+# creating the environment
+conda create -y -n ${CONDAENV} python=2.7
+# adding new conda packages
+conda install -y -n ${CONDAENV} numpy scipy matplotlib astropy sqlite
+# adding pip packages
+pip install pyfits
