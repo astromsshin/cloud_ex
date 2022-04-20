302b014f262352d5c350c0a3baf615c37f545cb5
Min-Su Shin <msshin@kasi.re.kr>
2022-04-20 13:38:57 +0900

    Example script to install apt packages on all nodes.

---
+++ tool_install_apt_packages_all_nodes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+pkgs=(astropy-utils python3-astropy python3-astropy-affiliated python3-astropy-healpix python3-astropy-helpers python3-sklearn python3-skimage python3-statsmodels python3-matplotlib)
+
+CLUSTERNAME="mycluster"
+MINIONLASTIND="14"
+
+echo "... install on ${CLUSTERNAME}-master"
+apt update -y
+for pkg in ${pkgs[@]}
+do
+  apt install $pkg -y
+done
+
+for ind in $(seq 0 ${MINIONLASTIND})
+do
+  echo "... install on ${CLUSTERNAME}-minion-${ind}"
+  ssh ${CLUSTERNAME}-minion-${ind} "apt update -y"
+  for pkg in ${pkgs[@]}
+  do
+    ssh ${CLUSTERNAME}-minion-${ind} "apt install ${pkg} -y"
+  done
+done
+
+
