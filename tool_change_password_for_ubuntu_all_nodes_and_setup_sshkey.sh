d1ad9885d3f91f0a523e57bbc930acc448ba676e
Min-Su Shin <msshin@kasi.re.kr>
2022-04-20 16:21:42 +0900

    Fixing some minor bugs.

--- tool_change_password_for_ubuntu_all_nodes_and_setup_sshkey.sh
+++ tool_change_password_for_ubuntu_all_nodes_and_setup_sshkey.sh
@@ -9,7 +9,6 @@ NEWPASSWORD="xxxxxxxxxx"
 
 NFSDIR="/mnt/mpi"
 
-
 # changing password of ubuntu account.
 
 echo "... changing ${CLUSTERNAME}-master : ${PWUSER}"
@@ -22,22 +21,25 @@ do
 done
 
 # generate ssh-key
-ssh-keygen -t ed25519 << ends_key
+rm -f ${NFSDIR}/id_ed25519 ${NFSDIR}/id_ed25519.pub
+### you should type empty passwords by entering twice.
+ssh-keygen -t ed25519 << endskey
 ${NFSDIR}/id_ed25519
-\r
-\r
-ends_key
+endskey
 
+# setup the environemnt for ssh access withouth password among the cluster nodes
+# for ubuntu account
 ### master
 echo "setup the master: ${CLUSTERNAME}-master"
-cp ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/
-cp ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/
+cp -f ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/
+cp -f ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/
 chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*
 chmod 600 /home/ubuntu/.ssh/id_ed25519
 chmod 644 /home/ubuntu/.ssh/id_ed25519.pub
+cat /home/ubuntu/.ssh/id_ed25519.pub >> /home/ubuntu/.ssh/authorized_keys
 ### slaves
 for ind in $(seq 0 ${MINIONLASTIND})
 do
   echo "setup the slave: ${CLUSTERNAME}-minion-${ind}"
-  ssh ${CLUSTERNAME}-minion-${ind} "cp ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/; cp ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/; chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*; chmod 600 /home/ubuntu/.ssh/id_ed25519; chmod 644 /home/ubuntu/.ssh/id_ed25519.pub"
+  ssh ${CLUSTERNAME}-minion-${ind} "cp -f ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/; cp -f ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/; chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*; chmod 600 /home/ubuntu/.ssh/id_ed25519; chmod 644 /home/ubuntu/.ssh/id_ed25519.pub; cat /home/ubuntu/.ssh/id_ed25519.pub >> /home/ubuntu/.ssh/authorized_keys"
 done
