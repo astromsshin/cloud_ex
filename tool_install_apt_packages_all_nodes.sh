7de905223a415199ec8840bb2dd92eb7ee932375
Min-Su Shin <msshin@kasi.re.kr>
2022-04-29 11:27:26 +0900

    Updating the comments.

--- tool_install_apt_packages_all_nodes.sh
+++ tool_install_apt_packages_all_nodes.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# list packages
-# search and check packages in https://packages.ubuntu.com/
-# for Ubuntu distributions
-pkgs=(astropy-utils python3-astropy python3-astropy-affiliated python3-astropy-healpix python3-astropy-helpers python3-sklearn python3-skimage python3-statsmodels python3-matplotlib python3-tqdm)
+### [IMPORTANT]
+# The root account/permission is required 
+# for installation of software by using apt pacakges.
+# You should run this script in root account.
+
+# List packages.
+# You can search and check packages in https://packages.ubuntu.com/
+# for Ubuntu distributions.
+pkgs=(astropy-utils python3-astropy python3-astropy-affiliated python3-astropy-healpix python3-astropy-helpers python3-sklearn python3-skimage python3-statsmodels python3-matplotlib python3-tqdm zip)
 
 CLUSTERNAME="mycluster"
 MINIONLASTIND="14"
