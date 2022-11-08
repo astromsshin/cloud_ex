5878c7d1dec2022c0b98a901263b9c7de27c5301
Min-Su Shin <msshin@kasi.re.kr>
2022-11-08 12:43:27 +0900

    Modifying to consider other possible network shared directory.

--- tool_change_password_for_ubuntu_all_nodes_and_setup_sshkey.sh
+++ tool_change_password_for_ubuntu_all_nodes_and_setup_sshkey.sh
@@ -7,7 +7,7 @@ MINIONLASTIND="4"
 PWUSER="ubuntu"
 NEWPASSWORD="xxxxxxxxxx"
 
-NFSDIR="/mnt/mpi"
+NETSHAREDIR="/mnt/mpi"
 
 # changing password of ubuntu account.
 
@@ -21,18 +21,18 @@ do
 done
 
 # generate ssh-key
-rm -f ${NFSDIR}/id_ed25519 ${NFSDIR}/id_ed25519.pub
+rm -f ${NETSHAREDIR}/id_ed25519 ${NETSHAREDIR}/id_ed25519.pub
 ### you should type empty passwords by entering twice.
 ssh-keygen -t ed25519 << endskey
-${NFSDIR}/id_ed25519
+${NETSHAREDIR}/id_ed25519
 endskey
 
 # setup the environemnt for ssh access withouth password among the cluster nodes
 # for ubuntu account
 ### master
 echo "setup the master: ${CLUSTERNAME}-master"
-cp -f ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/
-cp -f ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/
+cp -f ${NETSHAREDIR}/id_ed25519 /home/ubuntu/.ssh/
+cp -f ${NETSHAREDIR}/id_ed25519.pub /home/ubuntu/.ssh/
 chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*
 chmod 600 /home/ubuntu/.ssh/id_ed25519
 chmod 644 /home/ubuntu/.ssh/id_ed25519.pub
@@ -41,5 +41,5 @@ cat /home/ubuntu/.ssh/id_ed25519.pub >> /home/ubuntu/.ssh/authorized_keys
 for ind in $(seq 0 ${MINIONLASTIND})
 do
   echo "setup the slave: ${CLUSTERNAME}-minion-${ind}"
-  ssh ${CLUSTERNAME}-minion-${ind} "cp -f ${NFSDIR}/id_ed25519 /home/ubuntu/.ssh/; cp -f ${NFSDIR}/id_ed25519.pub /home/ubuntu/.ssh/; chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*; chmod 600 /home/ubuntu/.ssh/id_ed25519; chmod 644 /home/ubuntu/.ssh/id_ed25519.pub; cat /home/ubuntu/.ssh/id_ed25519.pub >> /home/ubuntu/.ssh/authorized_keys"
+  ssh ${CLUSTERNAME}-minion-${ind} "cp -f ${NETSHAREDIR}/id_ed25519 /home/ubuntu/.ssh/; cp -f ${NETSHAREDIR}/id_ed25519.pub /home/ubuntu/.ssh/; chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519*; chmod 600 /home/ubuntu/.ssh/id_ed25519; chmod 644 /home/ubuntu/.ssh/id_ed25519.pub; cat /home/ubuntu/.ssh/id_ed25519.pub >> /home/ubuntu/.ssh/authorized_keys"
 done
